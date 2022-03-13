23769ae6e9ba99171d346f3fb40e71765181f7a5
velavelavela <sadmir.hasanic@kitio.com>
2022-03-13 17:21:10 +0100

    create forms

---
+++ resources/views/admin/forms/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.home')
+
+@section('content')
+    <div class="events-header mb-3">
+
+        <button onclick="location.href='/dodaj-formo'" class="btn add-button"><span>Dodaj Formo</span><i
+                class="fas fa-plus"></i></button>
+
+    </div>
+@endsection
